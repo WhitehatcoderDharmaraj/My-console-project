a4b99efa97e11c45f0576e5f60fa4de7379c448e
Dharmaraj <90249634+WhitehatcoderDharmaraj@users.noreply.github.com>
2022-01-03 17:00:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
-console.log("My frist console project");
-consol.log("Randir Kapoor");
-console.log("15")
-console.log("5")
-console.log("50")
-console.log("2")
+var frist_name = "Dharmaraj";
+var middel_name = "Akshay";
+var last_name = "Lalgunkar";
+
+var full_name = frist_name + middel_name + last_name;
+
+console.log(full_name);
+
+
+var a = 10;
+var b = 5;
+var add = a+b;
+
+console.log(add);
+
+
+var c = 2.5;
+var d = 2.5;
+var add2 = c+d;
+
+console.log(add2);
+
+
+var e = 10;
+var f = 5;
+var add3 = e*f;
+
+console.log(add3);
+
+
+var g = 2;
+var h = 4;
+var add4 = g/h;
+
+console.log(add4);
